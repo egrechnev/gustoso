7b9fef0de61e141aea6bac8c1056a9e99455cbd9
egrechnev <egrechnev@gmail.com>
2019-01-27 02:30:41 +0400

    update

--- app/js/common.js
+++ app/js/common.js
@@ -2,45 +2,18 @@ $(function() {
 	$('.burger, .overlay, .navbar-mobile a').click(function(){
 	  $('.burger').toggleClass('clicked');
 	  $('.overlay').toggleClass('show');
-
-	  // $('.navbar-mobile').toggleClass('active');
 	  $('.navbar-mobile').slideToggle();
-	  // $(".navbar-mobile").animate({ width: "toggle" }, 300);
-
 	  $('body').toggleClass('overflow');
 	});
 
 	$(window).resize(function(){
 		if ($(window).width() > 992) {
-			// $('.navbar-mobile').removeClass('active');
 			$('.navbar-mobile').removeAttr('style');
 			$('.burger').removeClass('clicked');
 			$('.overlay').removeClass('show');
 			$('body').removeClass('overflow');
 		}
 	});
-
-
-///////
-
-    // $('.burger').on('click', function(e){
-    //     $('html').toggleClass('menu-active');
-    //     e.preventDefault();
-    // });
-
-//////
-
-
-//////
-
-
-
-/////
-
-
-
-/////
-
 
 
 //--Fake links
