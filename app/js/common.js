5979a51fef1d723e75205bb8560bc6e49cca7e70
egrechnev <egrechnev@gmail.com>
2019-02-07 14:31:46 +0400

    update

--- app/js/common.js
+++ app/js/common.js
@@ -1,9 +1,9 @@
 $(function() {
+	//--navbar
 	$('.burger, .overlay, .navbar-mobile a').click(function(){
 		$('.burger').toggleClass('clicked');
 		$('.overlay').toggleClass('show');
 		$('.navbar-mobile-wr').slideToggle(400);
-		// $('body').toggleClass('overflow');
 
 		if (!$(this).hasClass('clicked')) {
 			$('#noscroll').scrollLock('disable');
@@ -18,29 +18,28 @@ $(function() {
 			$('.navbar-mobile-wr').removeAttr('style');
 			$('.burger').removeClass('clicked');
 			$('.overlay').removeClass('show');
-			// $('body').removeClass('overflow');
 			$('#noscroll').scrollLock('disable');
 		}
 	});
 
-//--slick slides
-$('.slides').slick({
-	dots: false,
-	infinite: true,
-	slidesToShow: 3,
-	slidesToScroll: 3,
-	responsive: [
-	  {
-	    breakpoint: 410,
-	    settings: {
-	      slidesToShow: 2,
-	      slidesToScroll: 2
-	    }
-	  }
-	]
-});
+	//--slick slides
+	$('.slides').slick({
+		dots: false,
+		infinite: true,
+		slidesToShow: 3,
+		slidesToScroll: 3,
+		responsive: [
+		  {
+		    breakpoint: 410,
+		    settings: {
+		      slidesToShow: 2,
+		      slidesToScroll: 2
+		    }
+		  }
+		]
+	});
 
-//--Fake links
+	//--Fake links
 	$('a[data-fake-link]').click(function(e){
 		e.preventDefault();
 		// return false;
